b7dc9f6c1fbccfcf2f658e97025ccf9f0a2c1aa0
Joaquín León Quero <joaquinleonquero00@gmail.com>
2024-05-05 00:53:10 -0600

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Devstagram @yield('titulo')</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-white shadow p-5 border-b dark:bg-slate-900">
+    <h1 class="dark:text-white">Hola mundo</h1>
+</body>
+</html>
